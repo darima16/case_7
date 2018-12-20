1e9e2bc938f7508e9826eef066ebad63003b99ef
darima16 <ms.darima2000@mail.ru>
2018-12-21 00:42:51 +0700

    commit

--- main.py
+++ main.py
@@ -2,4 +2,31 @@
 Разработчики:
 Жамбаева Д., Зыкова К.
 
-""" 
+"""
+
+def read():
+    while True:
+        try:
+            file = input('Введите имя файла: ')
+            with open(file) as f_in:
+                text = f_in.read()
+                return text
+        except FileNotFoundError:
+            print('Файл не найден!'.format(file))
+def text_editing(text):
+    symbols = '@#$"<>{}^%;»«+*/[]^&'
+    for char in symbols:
+        text = text.replace(char, "")
+    for char in text:
+        text = text.replace(' .', ".")
+        text = text.replace(' !', "!")
+        text = text.replace(' ?', "?")
+        text = text.replace(' ,', ",")
+    return text
+
+
+
+
+
+
+
